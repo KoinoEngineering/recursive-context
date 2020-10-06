cf11153fb718f985e22b354e7cbe92763158ccb9
koinoengineering <email@koino.engineering>
2020-10-07 01:00:46 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -121,10 +121,18 @@ const ComponentReducer: React.FC<Props> = ({ components }) => {
     </Component>;
 };
 
+const style: React.CSSProperties = {
+    display: "inline-block",
+    width: "50%",
+    verticalAlign: "top",
+};
+
 const App: React.FC = () => {
     return <div>
-        <div style={{ display: "inline-block", width: "50%", verticalAlign: "top" }}>
-            順序に依存しないコンポーネント
+        <div style={style}>
+            <div>
+                順序に依存しないコンポーネント +1 -&gt; +1 -&gt; +2
+            </div>
             <div>
                 <AddOne>
                     <AddOne>
@@ -132,6 +140,18 @@ const App: React.FC = () => {
                         </AddTwo >
                     </AddOne>
                 </AddOne>
+            </div>
+            <br />
+            <div>
+                順序に依存しないコンポーネント +2 -&gt; +1 -&gt; +2
+            </div>
+            <div>
+                <AddTwo>
+                    <AddOne>
+                        <AddTwo>
+                        </AddTwo >
+                    </AddOne>
+                </AddTwo >
             </div>
             <br />
             非同期入れてみた
@@ -165,7 +185,7 @@ const App: React.FC = () => {
                 <ComponentReducer components={Array(100).fill(FizzBuzz)} />
             </div>
         </div>
-        <div style={{ display: "inline-block", width: "50%", verticalAlign: "top" }}>
+        <div style={style}>
             <div>
                 FizzBuzz非同期版
             </div>
